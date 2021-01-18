dea6eb3b04c246d65fd03a098bf32b8d7f983a82
aya abdelkader <75436836+aya9224@users.noreply.github.com>
2021-01-18 13:19:06 +0200

    music btn
    music btn

---
+++ musicbtn.js
@@ -0,0 +1,36 @@
+var audio= document.getElementById("audio");
+
+var backbutton= document.getElementById("musicbtn");
+
+backbutton.addEventListener("click",playButtonSound);
+
+function playButtonSound() {
+
+    audio.play();
+   
+    
+}
+
+
+var music= document.getElementById("music");
+
+var musictbtn = document.getElementById ("musicbtn");
+
+ startbtn = addEventListener("click",playMusic);
+
+
+
+function playMusic() {
+
+        
+  
+        
+        
+    music.play();
+
+
+    
+
+
+
+}
