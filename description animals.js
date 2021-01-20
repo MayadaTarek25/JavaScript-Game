74a1626d0704ec1a99e70f2979a848d67ee05ef3
aya abdelkader <75436836+aya9224@users.noreply.github.com>
2021-01-20 15:42:30 +0200

    description animals
    description animals

---
+++ description animals.js
@@ -0,0 +1,6 @@
+this.words = ["Lion", "Dog", "Cat", "Mouse", "Rabbit","Alephent" , "Crocodile" , "Frog" , "Turtle" , "Spider" , "Horse" , "Butterfly" , "Camel"];
+
+this.descriptions = ["King of forest", "Characterized by loyalty", "It loves milk",
+    "It loves cheese", "It is fast","It is large and grey" , "It eats fish and birds" , "Its skin is green and slippery" , 
+    "It carries its house around it" , "It has eight legs" , "It has long tail" , "It is small and colorful" , 
+    "It is a desert animal"]
